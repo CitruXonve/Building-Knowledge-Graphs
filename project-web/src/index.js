3d0ecbfb94b54eeb4bafd88d010596f919f3960d
CitruXonve <crxon@CitruXonves-MacBook-Pro.local>
2020-04-22 22:32:39 -0700

    Start visualization dev

--- project-web/src/index.js
+++ project-web/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/Main';
 
-ReactDOM.render(
+var App = () => (
+    <React.StrictMode>
+      <Main />
+    </React.StrictMode>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
